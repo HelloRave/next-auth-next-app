1fa38ba889d35e835cce8fa6632118685d2515b3
weiwei.huang <weiwei.huang@brtchip.com>
2023-12-19 14:20:27 +0800

    Update schema

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 
 export const signUpSchema = z.object({
+    name: z.string().min(3, 'Name at least 3 characters'),
     email: z.string().email(),
     password: z.string().min(6, 'Password at least 6 characters'),
     confirmPassword: z.string(),
