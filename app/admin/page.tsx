efcbfdf7e95dd1e8e183f854311a07467417b826
weiwei.huang <weiwei.huang@brtchip.com>
2024-01-03 17:15:16 +0800

    Style register form

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,16 @@
 import HookForm from "@/components/HookForm";
-import UserForm from "@/components/UserForm";
 
 export default function AdminPage() {
     return (
-        <>
-            <UserForm />
-            <HookForm />
-        </>
+        <div className="flex flex-col min-h-full justify-center">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                <h2 className="mt-10 text-center text-2xl font-bold text-gray-900">
+                    Sign up for an account
+                </h2>
+            </div>
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <HookForm />
+            </div>
+        </div>
     )
 }
--- components/HookForm.tsx
+++ components/HookForm.tsx
@@ -34,60 +34,89 @@ export default function HookForm() {
     })
 
     return (
-        <form onSubmit={onSubmit} className="flex flex-col gap-3 w-1/2">
-            <input
-                {...register('name')}
-            />
-            {
-                errors.name && (
-                    <p className="text-red-500">{errors.name.message}</p>
-                )
-            }
-            <input
-                {...register('email')}
-            />
-            {
-                errors.email && (
-                    <p className="text-red-500">{errors.email.message}</p>
-                )
-            }
-            <input
-                {...register('password')}
-            />
-            {/* <input
-                {...register('password', {
-                    required: 'Password is required',
-                    minLength: {
-                        value: 6,
-                        message: 'Password at least 6 characters',
-                    },
-                })}
-            /> */}
-            {
-                errors.password && (
-                    <p className="text-red-500">{errors.password.message}</p>
-                )
-            }
-            <input
-                {...register('confirmPassword')}
-            />
-            {/* <input
-                {...register('confirmPassword', {
-                    required: 'Confirm password is required',
-                    validate: (value) => {
-                        return value === getValues('password') || 'Password must match'
-                    }
-                })}
-            /> */}
-            {
-                errors.confirmPassword && (
-                    <p className="text-red-500">{errors.confirmPassword.message}</p>
-                )
-            }
+        <form onSubmit={onSubmit} className="flex flex-col gap-3">
+            <div>
+                <label htmlFor="name" className="block text-sm font-medium text-gray-900">
+                    Name
+                </label>
+                <input
+                    className="block mt-2 w-full rounded-md border-0 p-1.5 text-gray-900
+                    shadow-sm ring-1 ring-inset ring-gray-300"
+                    {...register('name')}
+                />
+                {
+                    errors.name && (
+                        <p className="text-red-500">{errors.name.message}</p>
+                    )
+                }
+            </div>
+            <div>
+                <label htmlFor="email" className="block text-sm font-medium text-gray-900">
+                    Email
+                </label>
+                <input
+                    className="block mt-2 w-full rounded-md border-0 p-1.5 text-gray-900
+                    shadow-sm ring-1 ring-inset ring-gray-300"
+                    {...register('email')}
+                />
+                {
+                    errors.email && (
+                        <p className="text-red-500">{errors.email.message}</p>
+                    )
+                }
+            </div>
+            <div>
+                <label htmlFor="password" className="block text-sm font-medium text-gray-900">
+                    Password
+                </label>
+                <input
+                    className="block mt-2 w-full rounded-md border-0 p-1.5 text-gray-900
+                    shadow-sm ring-1 ring-inset ring-gray-300"
+                    {...register('password')}
+                />
+                {/* <input
+                    {...register('password', {
+                        required: 'Password is required',
+                        minLength: {
+                            value: 6,
+                            message: 'Password at least 6 characters',
+                        },
+                    })}
+                /> */}
+                {
+                    errors.password && (
+                        <p className="text-red-500">{errors.password.message}</p>
+                    )
+                }
+            </div>
+            <div>
+                <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-900">
+                    Confirm Password
+                </label>
+                <input
+                    className="block mt-2 w-full rounded-md border-0 p-1.5 text-gray-900
+                    shadow-sm ring-1 ring-inset ring-gray-300"
+                    {...register('confirmPassword')}
+                />
+                {/* <input
+                    {...register('confirmPassword', {
+                        required: 'Confirm password is required',
+                        validate: (value) => {
+                            return value === getValues('password') || 'Password must match'
+                        }
+                    })}
+                /> */}
+                {
+                    errors.confirmPassword && (
+                        <p className="text-red-500">{errors.confirmPassword.message}</p>
+                    )
+                }
+            </div>
             <button
                 disabled={isSubmitting}
                 type="submit"
-                className="bg-blue-500 disable:bg-gray-500 py-2 rounded"
+                className="bg-indigo-600 mt-10 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm 
+                hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 rounded"
             >
                 Submit
             </button>
