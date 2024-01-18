3cc4e265c9deff7ad018ce1326512ba0828ee1aa
weiwei.huang <weiwei.huang@brtchip.com>
2024-01-18 15:58:40 +0800

    Format register form

--- components/RegisterHookForm.tsx
+++ components/RegisterHookForm.tsx
@@ -1,10 +1,11 @@
 'use client'
 
+import { Fragment } from "react";
 import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod";
 
 import { registerUser } from "@/app/admin/actions";
 import { TSignupSchema, signUpSchema } from "@/lib/schema";
-import { zodResolver } from "@hookform/resolvers/zod";
 import InputGroup from "./InputGroup";
 import SocialButton from "./SocialButton";
 import { GithubIcon, GoogleIcon } from "@/public/icons";
@@ -38,70 +39,74 @@ export default function RegisterHookForm() {
     })
 
     return (
-        <form onSubmit={onSubmit} className="flex flex-col gap-3">
-            <div>
-                <InputGroup<TSignupSchema>
-                    label="Name"
-                    name="name"
-                    type="text"
-                    register={register}
-                    errors={errors}
-                />
+        <Fragment>
+            <form onSubmit={onSubmit} className="flex flex-col gap-3">
+                <div>
+                    <InputGroup<TSignupSchema>
+                        label="Name"
+                        name="name"
+                        type="text"
+                        register={register}
+                        errors={errors}
+                    />
+                </div>
+                <div>
+                    <InputGroup<TSignupSchema>
+                        label="Email"
+                        name="email"
+                        type="email"
+                        register={register}
+                        errors={errors}
+                    />
+                </div>
+                <div>
+                    <InputGroup<TSignupSchema>
+                        label="Password"
+                        name="password"
+                        type="password"
+                        register={register}
+                        errors={errors}
+                    />
+                </div>
+                <div>
+                    <InputGroup<TSignupSchema>
+                        label="Confirm Password"
+                        name="confirmPassword"
+                        type="password"
+                        register={register}
+                        errors={errors}
+                    />
+                </div>
+                <button
+                    disabled={!isValid || isSubmitting}
+                    type="submit"
+                    className="bg-indigo-600 mt-10 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm
+                    disabled:bg-slate-300
+                    hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 rounded"
+                >
+                    Submit
+                </button>
+            </form>
+            <div className="mt-5 flex flex-col items-center gap-3">
+                <SocialButton
+                    title="Continue with GitHub"
+                    type="button"
+                    className="bg-black text-white hover:opacity-75"
+                    provider="github"
+                    callbackurl="/"
+                >
+                    <GithubIcon />
+                </SocialButton>
+                <SocialButton
+                    title="Continue with Google"
+                    type="button"
+                    className="bg-white text-black hover:opacity-75"
+                    provider="google"
+                    callbackurl="/"
+                >
+                    <GoogleIcon />
+                </SocialButton>
             </div>
-            <div>
-                <InputGroup<TSignupSchema>
-                    label="Email"
-                    name="email"
-                    type="email"
-                    register={register}
-                    errors={errors}
-                />
-            </div>
-            <div>
-                <InputGroup<TSignupSchema>
-                    label="Password"
-                    name="password"
-                    type="password"
-                    register={register}
-                    errors={errors}
-                />
-            </div>
-            <div>
-                <InputGroup<TSignupSchema>
-                    label="Confirm Password"
-                    name="confirmPassword"
-                    type="password"
-                    register={register}
-                    errors={errors}
-                />
-            </div>
-            <button
-                disabled={!isValid || isSubmitting}
-                type="submit"
-                className="bg-indigo-600 mt-10 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm
-                disabled:bg-slate-300
-                hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 rounded"
-            >
-                Submit
-            </button>
-            <SocialButton
-                title="Continue with GitHub"
-                type="button"
-                className="bg-black text-white hover:opacity-75"
-                provider="github"
-                callbackurl="/"
-            >
-                <GithubIcon />
-            </SocialButton>
-            <SocialButton
-                title="Continue with Google"
-                type="button"
-                className="bg-white text-black hover:opacity-75"
-                provider="google"
-                callbackurl="/"
-            >
-                <GoogleIcon />
-            </SocialButton>
-        </form>
+        </Fragment>
     )
 }
